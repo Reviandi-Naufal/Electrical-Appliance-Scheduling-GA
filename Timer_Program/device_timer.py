4fe4c02649fc3e60f2443889a1af062de93fcdbe
Reviandi Naufal Kurniawan <58059161+Reviandi-Naufal@users.noreply.github.com>
2022-04-26 13:24:28 +0700

    Update device_timer.py
    change some variables to be more descriptive and add 2 method to save time the device has been used

--- Timer_Program/device_timer.py
+++ Timer_Program/device_timer.py
@@ -11,63 +11,86 @@ class Device():
     def __init__(self):
         self.name = ""
         # self.clock = clock*3600 #convert time in hours to seconds
-        self.clock = 0
-        self.state = 0
+        self.time_scheduled = 0
+        self.time_used = 0
+        self.status = 0
     
-    def EnterData(self, name, clock, state):
+    def EnterData(self, name, time_scheduled, status):
         self.name = name
-        self.clock = clock
-        self.state = state
+        self.time_scheduled = time_scheduled
+        self.status = status
         
     def get_name(self):
         return self.name
     
-    def get_time(self):
-        return self.clock
+    def get_time_scheduled(self):
+        return self.time_scheduled
     
-    def get_state(self):
-        return self.state
+    def get_time_used(self):
+        return self.time_used
     
-    def set_state(self, state):
-        self.state = state
+    def set_time_used(self):
+        self.time_used += 1
+    
+    def get_status(self):
+        return self.status
+    
+    def set_status(self, status):
+        self.status = status
         
     def Display(self):
-        print(f"Device Name  : {self.name}")
-        print(f"Device Clock : {self.clock}")
-        print(f"Device State : {self.state}")
+        print(f"Device Name               : {self.name}")
+        print(f"Time Scehduled for Device : {self.time_scheduled}")
+        print(f"Time Used by Device       : {self.time_used}")
+        print(f"Device State              : {self.status}")
 
 if __name__ == '__main__' :
     
     name = ['device_1', 'device_2', 'device_3', 'device_4', 'device_5', 'device_6', 'device_7', 'device_8', 'device_9', 'device_10', 'device_11']
     schedule = [24, 23, 20, 16, 14, 12, 10, 9, 7, 5, 2]
-    state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    status = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     save_dev = []
     for i in range(len(schedule)):
         device = Device()
-        device.EnterData(name[i], schedule[i], state[i])
+        device.EnterData(name[i], schedule[i], status[i])
         save_dev.append(device)
-    """
+    
     for i in range(len(save_dev)):
         print("----------------------------")
         save_dev[i].Display()
         print("----------------------------")
-        """
+        
     print("SYSTEM ARE ON")
     print("========================================================================")
+    for device in range(len(save_dev)):
+        save_dev[device].set_status(1)
     startSys = monotonic()
     counter = len(save_dev)
     
     while counter != 0:
         endSys = monotonic()
         
-        for i in range(len(save_dev)):
-            if round(endSys - startSys) == save_dev[i].get_time():
-                save_dev[i].set_state(1)
-                print(save_dev[i].get_name(), "is OFF")
+        for device in range(len(save_dev)):
+            if round(endSys - startSys) == 8 and device == 6:
+                save_dev[device].set_status(0)
+                print(save_dev[device].get_name(), "is OFF")
                 counter -= 1
+                
+            elif save_dev[device].get_time_used() == save_dev[device].get_time_scheduled() and save_dev[device].get_status() != 0:
+                save_dev[device].set_status(0)
+                print(save_dev[device].get_name(), "is OFF")
+                counter -= 1
+                
+            elif save_dev[device].get_status() != 0 :
+                save_dev[device].set_time_used()
         
         sleep(1)
     
     print("========================================================================")
     print("ALl system has been shutdown")
     print(endSys - startSys)
+    
+    for i in range(len(save_dev)):
+        print("----------------------------")
+        save_dev[i].Display()
+        print("----------------------------")
